990c03e6e22e457e38389562213cfe1dfc220dda
Matt Friedman <maf675@gmail.com>
2016-10-20 17:46:59 -0700

    Add a simple functional test

---
+++ tests/functional/functional_test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Team Security Measures extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace phpbb\teamsecurity\tests\functional;
+
+/**
+ * @group functional
+ */
+class functional_test extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('phpbb/teamsecurity');
+	}
+
+	public function test_acp_modules()
+	{
+		$this->add_lang_ext('phpbb/teamsecurity', 'acp_teamsecurity');
+
+		$this->login();
+		$this->admin_login();
+
+		$crawler = self::request('GET', "adm/index.php?i=\\phpbb\\teamsecurity\\acp\\teamsecurity_module&mode=settings&sid={$this->sid}");
+		$this->assertContainsLang('ACP_TEAM_SECURITY_SETTINGS', $crawler->filter('#main h1')->text());
+	}
+}
