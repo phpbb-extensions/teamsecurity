c5448d8bd40447eaa929a2cafad77145ba7533df
Matt Friedman <maf675@gmail.com>
2025-06-08 10:27:58 -0700

    Update installation requirement enforcement

--- ext.php
+++ ext.php
@@ -20,14 +20,14 @@ class ext extends \phpbb\extension\base
 	 * The current phpBB version should meet or exceed
 	 * the minimum version required by this extension:
 	 *
-	 * Requires phpBB 3.2.0.
+	 * Requires phpBB 4.0.0 due to usage of the new messenger class
 	 *
 	 * @return bool
 	 * @access public
 	 */
 	public function is_enableable()
 	{
-		return phpbb_version_compare(PHPBB_VERSION, '3.2.0', '>=');
+		return phpbb_version_compare(PHPBB_VERSION, '4.0.0-dev', '>=') && phpbb_version_compare(PHPBB_VERSION, '4.1.0-dev', '<');
 	}
 
 	/**
